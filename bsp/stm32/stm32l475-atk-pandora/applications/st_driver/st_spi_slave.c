735344857a0431b46d505ebab8bf6d4b7dd58fde
windinjuries <294394591@qq.com>
2025-11-16 15:29:35 +0800

    fix: spi comm fail

--- bsp/stm32/stm32l475-atk-pandora/applications/st_driver/st_spi_slave.c
+++ bsp/stm32/stm32l475-atk-pandora/applications/st_driver/st_spi_slave.c
@@ -7,6 +7,12 @@
 #include "stm32l4xx_hal_gpio.h"
 #include "stm32l4xx_hal_i2c.h"
 #include "stm32l4xx_hal_spi.h"
+#include "main.h"
+
+#include <rtthread.h>
+#include <rtdevice.h>
+#include <board.h>
+#include <drv_gpio.h>
 
 SPI_HandleTypeDef hspi2;
 
@@ -24,42 +30,63 @@ uint8_t slow_table[128];
 
 typedef enum
 {
-    SPI_CMD_FAST_READ = 0,
+    SPI_CMD_FAST_READ = 0x00,
     SPI_CMD_SLOW_READ,
 } SPI_CMD;
 
 void ProcessModbusFrame(uint8_t *frame, uint16_t length);
 
-// void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
-// {
-//     if (GPIO_Pin == GPIO_PIN_4) 
-//     { 
-//         // CS引脚
-//         if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_4) == GPIO_PIN_RESET) 
-//         {
-//             // CS下降沿：开始接收数据
-//             rxIndex = 0;
-//             HAL_SPI_Receive_IT(&hspi1, &rxBuffer[rxIndex], 1);
-//         } 
-//         else 
-//         {
-//             // CS上升沿：接收完成，处理数据
-//             rxComplete = 1;
-//             HAL_SPI_Abort(&hspi1); // 停止SPI接收
-//         }
-//     }
-// }
+ void SPI2_CS_GPIO_Callback()
+
+ {
+    rt_uint8_t level = rt_pin_read(GET_PIN(B, 12)); // 读取当前电平
+    
+    if (level == PIN_HIGH)
+    {
+        HAL_SPI_Abort(&hspi2);
+    }
+    else // level == PIN_LOW
+    {
+		 HAL_SPI_TransmitReceive_IT(&hspi2, fast_table, rxBuffer, 128); 
+    }
+ }
 
 void HAL_SPI_RxCpltCallback(SPI_HandleTypeDef *hspi)
 {
     if (hspi->Instance == SPI2) 
     {
-        if(rxBuffer[0] == SPI_CMD_FAST_READ)
-        {
-            HAL_SPI_TransmitReceive_IT(&hspi2, fast_table, rxBuffer + 1, 128); 
-            rxIndex = 0;
-        }
+//        if(rxBuffer[0] == SPI_CMD_FAST_READ)
+//        {
+//            HAL_SPI_TransmitReceive_IT(&hspi2, fast_table, rxBuffer + 1, 127); 
+//        }
+//				else
+//				{
+//						HAL_SPI_TransmitReceive_IT(&hspi2, fast_table, rxBuffer + 1, 127); 
+//				} 
+//				HAL_SPI_TransmitReceive_IT(&hspi2, fast_table, rxBuffer, 128); 
     }
+		
+}
+
+/******************************************************************************/
+/* STM32L4xx Peripheral Interrupt Handlers                                    */
+/* Add here the Interrupt Handlers for the used peripherals.                  */
+/* For the available peripheral interrupt handler names,                      */
+/* please refer to the startup file (startup_stm32l4xx.s).                    */
+/******************************************************************************/
+
+/**
+  * @brief This function handles SPI2 global interrupt.
+  */
+void SPI2_IRQHandler(void)
+{
+  /* USER CODE BEGIN SPI2_IRQn 0 */
+
+  /* USER CODE END SPI2_IRQn 0 */
+  HAL_SPI_IRQHandler(&hspi2);
+  /* USER CODE BEGIN SPI2_IRQn 1 */
+
+  /* USER CODE END SPI2_IRQn 1 */
 }
 
 int st_spi_slave_init(void)
@@ -67,7 +94,7 @@ int st_spi_slave_init(void)
     hspi2.Instance            = SPI2;
     hspi2.Init.Mode           = SPI_MODE_SLAVE;
     hspi2.Init.Direction      = SPI_DIRECTION_2LINES;
-    hspi2.Init.DataSize       = SPI_DATASIZE_4BIT;
+    hspi2.Init.DataSize       = SPI_DATASIZE_8BIT;
     hspi2.Init.CLKPolarity    = SPI_POLARITY_LOW;
     hspi2.Init.CLKPhase       = SPI_PHASE_1EDGE;
     hspi2.Init.NSS            = SPI_NSS_SOFT;
@@ -77,12 +104,21 @@ int st_spi_slave_init(void)
     hspi2.Init.CRCPolynomial  = 7;
     hspi2.Init.CRCLength      = SPI_CRC_LENGTH_DATASIZE;
     hspi2.Init.NSSPMode       = SPI_NSS_PULSE_DISABLE;
+	
+		
+
     if (HAL_SPI_Init(&hspi2) != HAL_OK) 
     {
         return -1;
     }
-
-    HAL_SPI_Receive_IT(&hspi2, &rxBuffer[0], 1);
+		
+		// CS
+		rt_pin_mode(GET_PIN(B, 12), PIN_MODE_INPUT_PULLUP);
+	
+		rt_pin_attach_irq(GET_PIN(B, 12), PIN_IRQ_MODE_FALLING, SPI2_CS_GPIO_Callback, NULL);
+	
+		/* 使能引脚中断 */
+		rt_pin_irq_enable(GET_PIN(B, 12), PIN_IRQ_ENABLE);
 		return 0;
 }
 
@@ -93,7 +129,7 @@ void st_spi_slave_poll(void)
     {
         fast_table[i] = i;
     }
-    st_spi_slave_init();
+     st_spi_slave_init();
     while(1)
     {
 
--- bsp/stm32/stm32l475-atk-pandora/board/CubeMX_Config/Src/stm32l4xx_it.c
+++ bsp/stm32/stm32l475-atk-pandora/board/CubeMX_Config/Src/stm32l4xx_it.c
@@ -71,6 +71,7 @@
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
+extern SPI_HandleTypeDef hspi2;
 extern PCD_HandleTypeDef hpcd_USB_OTG_FS;
 /* USER CODE BEGIN EV */
 
@@ -211,6 +212,20 @@ void SysTick_Handler(void)
 /* For the available peripheral interrupt handler names,                      */
 /* please refer to the startup file (startup_stm32l4xx.s).                    */
 /******************************************************************************/
+
+/**
+  * @brief This function handles SPI2 global interrupt.
+  */
+void SPI2_IRQHandler(void)
+{
+  /* USER CODE BEGIN SPI2_IRQn 0 */
+
+  /* USER CODE END SPI2_IRQn 0 */
+  HAL_SPI_IRQHandler(&hspi2);
+  /* USER CODE BEGIN SPI2_IRQn 1 */
+
+  /* USER CODE END SPI2_IRQn 1 */
+}
 
 /**
   * @brief This function handles USB OTG FS global interrupt.
