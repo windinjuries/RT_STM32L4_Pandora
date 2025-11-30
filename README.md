b9a5182180d2cef81dd5c1b873ef7e1c9d6f7fe5
windinjuries <294394591@qq.com>
2025-11-30 11:05:06 +0800

    feat: spi comm add crc16

--- README.md
+++ README.md
@@ -26,3 +26,20 @@ scons --target=mdk5
 1. i2c
 2. sdio
 
+
+## hardware
+
+### spi 
+STM32L4
+1. IO_PC6  SPI_CS
+2. IO_PB13 SPI2_SCK
+3. IO_PB14 SPI2_MISO
+4. IO_PB15 SPI2_MOSI
+
+orangepi
+1. DIP26-3 SPI1_CS0
+2. DIP26-24 SPI1_CS1
+3. DIP26-23 SPI1_CLK
+4. DIP26-21 SPI1_MISO
+5. DIP26-19 SPI1_MOSI
+
--- bsp/stm32/stm32l475-atk-pandora/applications/st_driver/st_spi_slave.c
+++ bsp/stm32/stm32l475-atk-pandora/applications/st_driver/st_spi_slave.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#include "drivers/dev_pin.h"
 #include "stm32l475xx.h"
 #include "stm32l4xx.h"
 #include "stm32l4xx_hal.h"
+#include "stm32l4xx_hal_crc.h"
 #include "stm32l4xx_hal_gpio.h"
 #include "stm32l4xx_hal_i2c.h"
 #include "stm32l4xx_hal_spi.h"
@@ -14,9 +16,11 @@
 #include <board.h>
 #include <drv_gpio.h>
 
+// SPI handle
 SPI_HandleTypeDef hspi2;
-
-/* Modbus相关变量 */
+// CRC handle
+CRC_HandleTypeDef hcrc;                                                  
+#define CS_PIN GET_PIN(C, 6)
 #define SLAVE_ADDRESS 0x01
 #define MAX_FRAME_LEN 256
 uint8_t rxBuffer[MAX_FRAME_LEN];
@@ -25,8 +29,14 @@ volatile uint16_t rxIndex = 0;
 volatile uint8_t rxComplete = 0;
 volatile uint8_t txInProgress = 0;
 
-uint8_t fast_table[128];
-uint8_t slow_table[128];
+uint32_t cs_count = 0;
+uint32_t cs_low_count = 0;
+uint32_t cs_high_count = 0;
+uint32_t crc_error_count = 0;
+uint32_t crc_cal_count = 0;
+
+uint8_t fast_table[MAX_FRAME_LEN];
+uint8_t slow_table[MAX_FRAME_LEN];
 
 typedef enum
 {
@@ -34,63 +44,68 @@ typedef enum
     SPI_CMD_SLOW_READ,
 } SPI_CMD;
 
-void ProcessModbusFrame(uint8_t *frame, uint16_t length);
 
- void SPI2_CS_GPIO_Callback()
-
- {
-    rt_uint8_t level = rt_pin_read(GET_PIN(B, 12)); // 读取当前电平
-    
-    if (level == PIN_HIGH)
+void SPI2_CS_GPIO_Callback()
+{
+    rt_uint8_t level = rt_pin_read(CS_PIN);
+    cs_count++;
+    if (level == PIN_LOW) 
     {
-        HAL_SPI_Abort(&hspi2);
-    }
-    else // level == PIN_LOW
+        HAL_SPI_TransmitReceive_IT(&hspi2, fast_table, rxBuffer, MAX_FRAME_LEN);
+        cs_low_count++;
+    } 
+    else 
     {
-		 HAL_SPI_TransmitReceive_IT(&hspi2, fast_table, rxBuffer, 128); 
+        HAL_SPI_Abort_IT(&hspi2);
     }
- }
+}
 
+void HAL_SPI_TxRxCpltCallback(SPI_HandleTypeDef *hspi)
+{
+    if (hspi->Instance == SPI2) 
+    {
+        uint16_t res = HAL_CRC_Calculate(&hcrc, (uint32_t*)rxBuffer, MAX_FRAME_LEN);
+        if(res != 0)
+        {
+            crc_error_count++;
+        }
+        crc_cal_count++;        
+
+    }
+}
+
+uint32_t rx_count = 0;
 void HAL_SPI_RxCpltCallback(SPI_HandleTypeDef *hspi)
 {
     if (hspi->Instance == SPI2) 
     {
-//        if(rxBuffer[0] == SPI_CMD_FAST_READ)
-//        {
-//            HAL_SPI_TransmitReceive_IT(&hspi2, fast_table, rxBuffer + 1, 127); 
-//        }
-//				else
-//				{
-//						HAL_SPI_TransmitReceive_IT(&hspi2, fast_table, rxBuffer + 1, 127); 
-//				} 
-//				HAL_SPI_TransmitReceive_IT(&hspi2, fast_table, rxBuffer, 128); 
+				rx_count++;
+
     }
-		
 }
 
-/******************************************************************************/
-/* STM32L4xx Peripheral Interrupt Handlers                                    */
-/* Add here the Interrupt Handlers for the used peripherals.                  */
-/* For the available peripheral interrupt handler names,                      */
-/* please refer to the startup file (startup_stm32l4xx.s).                    */
-/******************************************************************************/
-
-/**
-  * @brief This function handles SPI2 global interrupt.
-  */
-void SPI2_IRQHandler(void)
+static int st_crc_init(void)
 {
-  /* USER CODE BEGIN SPI2_IRQn 0 */
+    // Modbus CRC16
+    hcrc.Instance                     = CRC;
+    hcrc.Init.DefaultPolynomialUse    = DEFAULT_POLYNOMIAL_DISABLE;      // 不使用默认多项式
+    hcrc.Init.DefaultInitValueUse     = DEFAULT_INIT_VALUE_DISABLE;      // 不使用默认初始值
+    hcrc.Init.GeneratingPolynomial    = 0x8005;                          // 设置Modbus多项式
+    hcrc.Init.CRCLength               = CRC_POLYLENGTH_16B;              // 16位CRC
+    hcrc.Init.InitValue               = 0xFFFF;                          // 设置初始值
+    hcrc.Init.InputDataInversionMode  = CRC_INPUTDATA_INVERSION_BYTE;    // 按字节反转输入
+    hcrc.Init.OutputDataInversionMode = CRC_OUTPUTDATA_INVERSION_ENABLE; // 输出结果反转
+    hcrc.InputDataFormat              = CRC_INPUTDATA_FORMAT_BYTES;      // 按字节处理
 
-  /* USER CODE END SPI2_IRQn 0 */
-  HAL_SPI_IRQHandler(&hspi2);
-  /* USER CODE BEGIN SPI2_IRQn 1 */
-
-  /* USER CODE END SPI2_IRQn 1 */
+    if (HAL_CRC_Init(&hcrc) != HAL_OK) 
+    {
+        return -1;
+    }
 }
 
-int st_spi_slave_init(void)
+static int st_spi_slave_init(void)
 {
+    // SPI Init
     hspi2.Instance            = SPI2;
     hspi2.Init.Mode           = SPI_MODE_SLAVE;
     hspi2.Init.Direction      = SPI_DIRECTION_2LINES;
@@ -104,35 +119,49 @@ int st_spi_slave_init(void)
     hspi2.Init.CRCPolynomial  = 7;
     hspi2.Init.CRCLength      = SPI_CRC_LENGTH_DATASIZE;
     hspi2.Init.NSSPMode       = SPI_NSS_PULSE_DISABLE;
-	
-		
 
     if (HAL_SPI_Init(&hspi2) != HAL_OK) 
     {
         return -1;
     }
+    // CS Pin Init
+    rt_pin_mode(CS_PIN, PIN_MODE_INPUT_PULLUP);
+    rt_pin_attach_irq(CS_PIN, PIN_IRQ_MODE_RISING_FALLING, SPI2_CS_GPIO_Callback, NULL);
+    rt_pin_irq_enable(CS_PIN, PIN_IRQ_ENABLE);
 		
-		// CS
-		rt_pin_mode(GET_PIN(B, 12), PIN_MODE_INPUT_PULLUP);
-	
-		rt_pin_attach_irq(GET_PIN(B, 12), PIN_IRQ_MODE_FALLING, SPI2_CS_GPIO_Callback, NULL);
-	
-		/* 使能引脚中断 */
-		rt_pin_irq_enable(GET_PIN(B, 12), PIN_IRQ_ENABLE);
-		return 0;
+    return 0;
 }
 
+void SPI2_IRQHandler(void)
+{
+    HAL_SPI_IRQHandler(&hspi2);
+}
+
+extern uint32_t exti_count;
 void st_spi_slave_poll(void)
 {
-    /* prepare data */
-    for(int i = 0; i < 128; i++)
+    st_spi_slave_init();
+    st_crc_init();
+
+    /* prepare send data */
+    for(int i = 0; i < MAX_FRAME_LEN - 2; i++)
     {
-        fast_table[i] = i;
+        fast_table[i] = 0xBB;
     }
-     st_spi_slave_init();
+    uint16_t crc = HAL_CRC_Calculate(&hcrc, (uint32_t *)fast_table, MAX_FRAME_LEN -2);
+		/* swap crc byte */
+    fast_table[MAX_FRAME_LEN - 2] = crc & 0xFF;
+		fast_table[MAX_FRAME_LEN - 1] = (crc >> 8) & 0xFF;
+
+    crc = HAL_CRC_Calculate(&hcrc, (uint32_t *)fast_table, MAX_FRAME_LEN);
+    
     while(1)
     {
-
+        rt_kprintf("spi cs fall count: %d / %d\n", cs_low_count, cs_count);
+        rt_thread_mdelay(1000);
+        rt_kprintf("spi slave error count: %d / %d\n", crc_error_count, crc_cal_count);
+        rt_thread_mdelay(1000);
+			  rt_kprintf("exti count: %d\n", exti_count);
     }
 }
 
